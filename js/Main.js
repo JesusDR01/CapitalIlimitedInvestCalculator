695b08337a4102c0501d2e9e3a0de5ea09d71c63
bules <jesus20.11@hotmail.es>
2021-01-08 05:13:35 +0100

    Added:API centralBank, POO

--- js/Main.js
+++ js/Main.js
@@ -1,31 +1,40 @@
-function getMoney() {
-  var money = document.getElementById("moneyInput").value;
+class UserData {
 
-  return parseFloat(money);
-}
-
-function getDays() {
-  var days = document.getElementById("daysInput").value;
-
-  document.getElementById("daysWaiting").innerHTML = days;
-  return parseInt(days);
-}
-
-function calculatePercentage(userMoneyInvested) {
-  if (userMoneyInvested < 1000) {
-    percentage = 0.025;
-  } else {
-    percentage = 0.03;
+  constructor() {
+    this.money = this.getMoney();
+    this.days = this.getDays();
+    this.percentage = this.getPercentage();
   }
 
-  return percentage;
+  getMoney() {
+    var money = document.getElementById("moneyInput").value;
+    return parseFloat(money);
+  }
+
+  getDays() {
+    var days = document.getElementById("daysInput").value;
+    return parseInt(days);
+  }
+
+  getPercentage() {
+
+    if (this.money < 1000) {
+      this.percentage = 0.025;
+    } else {
+      this.percentage = 0.03;
+    }
+
+    return this.percentage;
+  }
+
 }
 
 function moneyCalculations() {
-  var currentMoney = getMoney();
-  var days = getDays();
-
-  var benefitsPercentagePerDay = calculatePercentage(currentMoney);
+  let myUser = new UserData();
+  
+  var currentMoney = myUser.getMoney();
+  var days = myUser.getDays();
+  var benefitsPercentagePerDay = myUser.getPercentage();
 
   for (i = 0; i < days; i += 6) {
     moneyGeneratedEveryDay = currentMoney * benefitsPercentagePerDay;
@@ -33,23 +42,41 @@ function moneyCalculations() {
     moneySum = currentMoney + moneyGeneratedPast6Days;
     currentMoney = moneySum;
   }
-
+  
   return currentMoney;
 }
 
-function printFinalMoney() {
+const api_url =
+  "https://api.exchangeratesapi.io/latest?base=USD";
+
+  const userAction = async () => {
+  const response = await fetch(api_url);
+  const data = await response.json();
+  return data['rates']['EUR'];
+};
+
+async function printFinalMoney() { 
+  
   var finalMoney = moneyCalculations();
+  var data = await userAction();
 
   document.getElementById("result").innerHTML = finalMoney.toFixed(2);
-  document.getElementById("resultEuros").innerHTML = (
-    finalMoney * 0.82
-  ).toFixed(2);
+  document.getElementById("resultEuros").innerHTML = (finalMoney * data).toFixed(2);
 }
 
-function deleteFinalMoney() {
-    var finalMoney = moneyCalculations();
-  
-    document.getElementById("result").innerHTML = "...";
-    document.getElementById("resultEuros").innerHTML =  "...";
-    document.getElementById("daysWaiting").innerHTML = "...";
-  }
+function printDaysWaiting(){
+  let myUser = new UserData();
+  var days = myUser.getDays();
+  document.getElementById("daysWaiting").innerHTML = days;
+}
+
+function printData(){
+  printFinalMoney();
+  printDaysWaiting();
+}
+
+function deleteData() {
+  document.getElementById("result").innerHTML = "...";
+  document.getElementById("resultEuros").innerHTML = "...";
+  document.getElementById("daysWaiting").innerHTML = "...";
+}
